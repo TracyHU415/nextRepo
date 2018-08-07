2fbe48bb15c13793b1253c55c528ccb334e2c327
GWC04 <tracyhu0321@gmail.com>
2018-08-07 14:48:01 -0400

    added index and js file

---
+++ map.js
@@ -0,0 +1,29 @@
+var ourLoc;
+var view;
+var map;
+
+
+function init() {
+    ourLoc = ol.proj.fromLonLat([41.043316, 28.862457]);
+
+    view = new ol.View({
+        center: ourLoc,
+        zoom: 6
+    });
+
+    map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        loadTilesWhileAnimating: true,
+        view:view
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function (e){
+    init();
+});
+
